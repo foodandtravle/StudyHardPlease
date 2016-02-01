3b308b5175dc1225db2dc6454d1bf8002f045c1a
张德强 <952528232@qq.com>
2016-02-01 23:00:41 +0800

    CacheDemo

---
+++ StudyHardPlease/FirstPage/CacheDemo/Cache.h
@@ -0,0 +1,13 @@
+//
+//  Cache.h
+//  StudyHardPlease
+//
+//  Created by 张德强 on 16/2/1.
+//  Copyright © 2016年 张德强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Cache : UIViewController
+
+@end
---
+++ StudyHardPlease/FirstPage/CacheDemo/Cache.m
@@ -0,0 +1,37 @@
+//
+//  Cache.m
+//  StudyHardPlease
+//
+//  Created by 张德强 on 16/2/1.
+//  Copyright © 2016年 张德强. All rights reserved.
+//
+
+#import "Cache.h"
+
+@interface Cache ()
+
+@end
+
+@implementation Cache
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
